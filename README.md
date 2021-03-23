2fcb3767bd3934d37df769f30ffd64f76a885e69
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-03-23 16:59:38 +0900

    Add audio_combiner for merging all audio segments for a chapter into a single file; WIP use different voice for dialogues

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Trasnsform documents to voice
 
-- Uses https://www.npmjs.com/package/node-gtts
+Based on:
+
+- https://www.npmjs.com/package/node-gtts
+- https://github.com/ttippin84/audio-combiner
 
 In order to transform the documents into a voice file for each:
+
+```
+npm install
+```
 
 ```
 node index.js
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var fs = require("fs");
+const fs = require("fs");
 const gTTS = require("gtts");
+const audioCombiner = require("./src/scripts/audio-combiner.js");
 
 const transformFile = (filename) => {
   const SOURCE_FOLDER = "src/";
@@ -7,17 +8,41 @@ const transformFile = (filename) => {
   const OUTPUT_FOLDER = SOURCE_FOLDER + "output/";
   const SPAIN_SPANISH = "es-es";
   const AMERICAN_SPANISH = "es-us";
+  const VOICES = [AMERICAN_SPANISH, SPAIN_SPANISH];
   const UTF_8 = "utf8";
 
-  const generateVoiceFile = (text, filename) => {
-    var gtts = new gTTS(text, SPAIN_SPANISH);
+  const generateVoiceFile = (text, filename, voice) => {
+    var gtts = new gTTS(text, voice);
 
-    gtts.save(`${OUTPUT_FOLDER}${filename}.mp3`, (error, _) => {
+    gtts.save(`${OUTPUT_FOLDER}${filename}`, (error, _) => {
       if (error) {
         throw new Error(error);
       }
       console.log(`${filename} transformed.`);
     });
+  };
+
+  const combineVoiceFiles = (filenames, outputFilename) => {
+    const combineFiles = (segmentFile) => {
+      audioCombiner.combineSamples(
+        `${OUTPUT_FOLDER}${segmentFile}`,
+        `${OUTPUT_FOLDER}${outputFilename}`,
+        `${OUTPUT_FOLDER}${outputFilename}`,
+        (error) => {
+          if (error) {
+            throw new Error(error);
+          }
+
+          console.log(
+            `${segmentFile} file was included into ${outputFilename}`
+          );
+        }
+      );
+    };
+
+    audioCombiner.soxPath = "./sox/sox";
+
+    filenames.forEach(combineFiles);
   };
 
   const readFile = (filename) => {
@@ -27,12 +52,32 @@ const transformFile = (filename) => {
       }
 
       console.log(`${filename} read.`);
-      generateVoiceFile(text, filename);
+
+      let segmentsFilenames = [];
+
+      const replaceAll = (string, search, replacement) =>
+        string.split(search).join(replacement);
+
+      //replaceAll(text, "\n", "-")
+      replaceAll(text, "–", "-")
+        .split("-")
+        .forEach((textSegment, index) => {
+          const isOdd = (number) => number % 2;
+
+          console.log(`${index}. ${textSegment}`);
+          /* const segmentFilename = `${filename}${
+            index === 0 ? "" : `_${index}`
+          }`;
+          segmentsFilenames.push(segmentFilename);
+          generateVoiceFile(textSegment, segmentFilename, VOICES[isOdd(index)]); */
+        });
+
+      //combineVoiceFiles(segmentsFilenames, filename);
     });
   };
 
   readFile(filename);
 };
 
-const filename = "cad1/cad1_cap2";
+const filename = "cad1/cad1_cap3";
 transformFile(filename);
