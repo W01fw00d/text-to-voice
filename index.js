808d273741877ed69c8ec174c1b7cdb7121c7f29
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-03-29 16:04:05 +0900

    Code clean; include disabled song feature (#11)
    Starts #4 
    * Use library fluent-ffmpeg for adding songs to output without corruption

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const fs = require("fs");
 const gTTS = require("gtts");
+const ffmpeg = require("fluent-ffmpeg");
 
-const transformFile = (filename, shallAddChapterNumber) => {
+const transformFile = () => {
   const SOURCE_FOLDER = "src/";
   const INPUT_FOLDER = SOURCE_FOLDER + "input/";
   const OUTPUT_FOLDER = SOURCE_FOLDER + "output/";
-  const FILE_EXTENSION = ".mp3";
+  const SONGS_FOLDER = `${INPUT_FOLDER}songs/`;
+
+  const AUDIO_EXTENSION = ".mp3";
+  const DOC_EXTENSION = ".txt";
   const SPAIN_SPANISH = "es-es";
   const AMERICAN_SPANISH = "es-us";
   const VOICES = [SPAIN_SPANISH, AMERICAN_SPANISH];
   const UTF_8 = "utf8";
 
+  // ---
+  const bookCode = "cad1";
+  const chapterCode = "cap15";
+  const shallAddChapterNumber = false;
+  // ---
+
+  const filename = `${bookCode}/${bookCode}_${chapterCode}`;
+  const openingSong = `${SONGS_FOLDER}opening/${bookCode}${AUDIO_EXTENSION}`;
+  const closureSong = `${SONGS_FOLDER}closure/${bookCode}${AUDIO_EXTENSION}`;
+
   const generateVoiceFile = (text, filename, voice, callback) => {
     var gtts = new gTTS(text, voice);
 
-    gtts.save(`${OUTPUT_FOLDER}${filename}${FILE_EXTENSION}`, (error, _) => {
+    gtts.save(`${OUTPUT_FOLDER}${filename}${AUDIO_EXTENSION}`, (error, _) => {
       if (error) {
         throw new Error(error);
       }
@@ -33,122 +47,119 @@ const transformFile = (filename, shallAddChapterNumber) => {
   };
 
   const combineVoiceFiles = (filenames, outputFilename) => {
-    console.log(
-      "combineVoiceFiles",
-      `${OUTPUT_FOLDER}${outputFilename}${FILE_EXTENSION}`
+    const TEMP_FOLDER = `${OUTPUT_FOLDER}${bookCode}/ffmpeg_temp`;
+    // TODO: add missing - to dialogues in .txt file
+    // TODO: implement deleting segment files, after every merge if possible
+    //TODO: can we print the full ffmpeg command? In case this API is lost...
+
+    const ffmpegInstanceWithInputs = filenames.reduce(
+      (accumulator, filename) => accumulator.input(filename),
+      ffmpeg()
     );
-    var fs = require("fs"),
-      stream,
-      currentFile,
-      dhh = fs.createWriteStream(
-        `${OUTPUT_FOLDER}${outputFilename}${FILE_EXTENSION}`
-      );
 
-    let index = 0;
-
-    const main = () => {
-      if (index >= filenames.length) {
-        console.log(`${outputFilename} transformed.`);
-        console.log("[Task completed]");
-        return;
-      }
-      currentFile = `${OUTPUT_FOLDER}` + filenames[index] + FILE_EXTENSION;
-      stream = fs.createReadStream(currentFile);
-      stream.pipe(dhh, { end: false });
-      stream.on("end", () => {
-        console.log(currentFile + " appended");
-        deleteSegmentFile(currentFile);
-        index++;
-        main();
-      });
-    };
-    main();
+    ffmpegInstanceWithInputs
+      .on("error", function (err) {
+        console.log("An error occurred: " + err.message);
+      })
+      .on("end", function () {
+        console.log("ffmpeg merging finished!");
+      })
+      .mergeToFile(outputFilename, TEMP_FOLDER);
   };
 
   const readFile = (filename, shallAddChapterNumber) => {
-    fs.readFile(`${INPUT_FOLDER}${filename}`, UTF_8, (error, text) => {
-      if (error) {
-        throw new Error(error);
-      }
+    fs.readFile(
+      `${INPUT_FOLDER}${filename}${DOC_EXTENSION}`,
+      UTF_8,
+      (error, text) => {
+        if (error) {
+          throw new Error(error);
+        }
 
-      const addChapterNumber = (text) => {
-        const splittedFilename = filename.split("_");
-        const chapterNumber = splittedFilename[
-          splittedFilename.length - 1
-        ].replace("cap", "");
-        text = `Capítulo ${chapterNumber}: ` + text;
+        const addChapterNumber = (text) => {
+          const splittedFilename = filename.split("_");
+          const chapterNumber = splittedFilename[
+            splittedFilename.length - 1
+          ].replace("cap", "");
+          text = `Capítulo ${chapterNumber}: ` + text;
 
-        return text;
-      };
+          return text;
+        };
 
-      console.log(`${filename} read succesfully.`);
+        console.log(`${filename} read succesfully.`);
 
-      let segmentsFilenames = [];
+        let segmentsFilenames = [openingSong];
 
-      const replaceAll = (string, search, replacement) =>
-        string.split(search).join(replacement);
+        const replaceAll = (string, search, replacement) =>
+          string.split(search).join(replacement);
 
-      let currentVoiceIndex = 1;
-      let textSegmentIndex = 0;
-      let textSubSegmentIndex = 0;
+        let currentVoiceIndex = 1;
+        let textSegmentIndex = 0;
+        let textSubSegmentIndex = 0;
 
-      if (shallAddChapterNumber) {
-        text = addChapterNumber(text);
-      }
+        if (shallAddChapterNumber) {
+          text = addChapterNumber(text);
+        }
 
-      const segmentArray = replaceAll(text, "–", "-").split("\n");
+        const segmentArray = replaceAll(text, "–", "-").split("\n");
 
-      const iterate = () => {
-        const callback = () => {
-          textSubSegmentIndex++;
+        const iterate = () => {
+          const callback = () => {
+            textSubSegmentIndex++;
 
-          if (textSubSegmentIndex < subSegmentArray.length) {
-            iterate();
-          } else {
-            textSegmentIndex++;
-
-            if (textSegmentIndex < segmentArray.length) {
-              textSubSegmentIndex = 0;
+            if (textSubSegmentIndex < subSegmentArray.length) {
               iterate();
             } else {
-              // End of iteration
-              combineVoiceFiles(segmentsFilenames, filename);
+              textSegmentIndex++;
+
+              if (textSegmentIndex < segmentArray.length) {
+                textSubSegmentIndex = 0;
+                iterate();
+              } else {
+                // End of iteration
+                segmentsFilenames.push(closureSong);
+
+                //console.log("segmentsFilenames", segmentsFilenames);
+
+                combineVoiceFiles(
+                  segmentsFilenames,
+                  `${OUTPUT_FOLDER}${filename}_ffmpeg${AUDIO_EXTENSION}`
+                );
+              }
             }
+          };
+
+          const subSegmentArray = segmentArray[textSegmentIndex].split("-");
+          const textSubSegment = subSegmentArray[textSubSegmentIndex];
+          if (textSubSegment && textSubSegment.trim().length > 0) {
+            const isOdd = (number) => number % 2;
+
+            currentVoiceIndex = isOdd(textSubSegmentIndex);
+
+            const segmentFilename = `${filename}_${textSegmentIndex}_${textSubSegmentIndex}`;
+            segmentsFilenames.push(
+              `${OUTPUT_FOLDER}${segmentFilename}${AUDIO_EXTENSION}`
+            );
+            //console.log(`${currentVoiceIndex}. ${textSubSegment}`); //
+            //callback(); //
+            generateVoiceFile(
+              //
+              textSubSegment,
+              segmentFilename,
+              VOICES[currentVoiceIndex],
+              callback
+            ); //
+          } else {
+            callback();
           }
         };
 
-        const subSegmentArray = segmentArray[textSegmentIndex].split("-");
-        const textSubSegment = subSegmentArray[textSubSegmentIndex];
-        if (textSubSegment && textSubSegment.trim().length > 0) {
-          const isOdd = (number) => number % 2;
-
-          currentVoiceIndex = isOdd(textSubSegmentIndex);
-
-          const segmentFilename = `${filename}_${textSegmentIndex}_${textSubSegmentIndex}`;
-          segmentsFilenames.push(segmentFilename);
-          //console.log(`${currentVoiceIndex}. ${textSubSegment}`); //
-          //callback(); //
-          generateVoiceFile(
-            textSubSegment,
-            segmentFilename,
-            VOICES[currentVoiceIndex],
-            callback
-          );
-        } else {
-          callback();
-        }
-      };
-
-      iterate();
-    });
+        iterate();
+      }
+    );
   };
-
-  // ---
 
   readFile(filename, shallAddChapterNumber);
 };
 
-// ----------------------------
-
-const shallAddChapterNumber = true;
-transformFile("cad1/cad1_cap13", shallAddChapterNumber);
+transformFile();
