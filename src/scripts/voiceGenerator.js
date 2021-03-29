6493f5b9372933c80fa5c40428513d0a601588ea
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-03-29 19:13:18 +0900

    Improve debugging console.logs

--- src/scripts/voiceGenerator.js
+++ src/scripts/voiceGenerator.js
@@ -1,8 +1,8 @@
 module.exports = (text, filename, voice, callback) => {
   const gTTS = require("gtts");
 
-  //console.log(`${voice}: ${text}`); //
-  //callback(); //
+  // console.log(`[${filename}] (${voice}): ${text}`); //
+  // callback(); //
   new gTTS(text, voice).save(filename, (error, _) => {
     if (error) {
       throw new Error(error);
