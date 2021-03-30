17b2691afcc8e9f0b1c9f02dfa52633f2a6c70be
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-03-30 21:46:40 +0900

    Fixes #8; sttutering - is transformed into ... to avoid confusion with dialogue delimiters

--- src/scripts/textManipulator.js
+++ src/scripts/textManipulator.js
@@ -3,9 +3,13 @@ const replaceAll = (string, search, replacement) =>
 
 exports.getTextArrayFormatted = (text) =>
   replaceAll(
-    replaceAll(text, "–", "-") /* Unify dialogue delimiters. */,
-    /\*{4,}/ /* Reduce asterisk delimiters to avoid repetitive voice */,
-    "***"
+    replaceAll(
+      replaceAll(text, "–", "-") /* Unify dialogue delimiters */,
+      /\*{4,}/ /* Reduce asterisk delimiters to avoid repetitive voice */,
+      "***"
+    ),
+    /(?<=[A-z])\-(?=[A-z])/,
+    "..." /* Gets "-"" used to express stuttering and replace them by "..." to avoid confusion with dialogue delimiters */
   ).split("\n");
 
 exports.addChapterNumber = (text) => {
